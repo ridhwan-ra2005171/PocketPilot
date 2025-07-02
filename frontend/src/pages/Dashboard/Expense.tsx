c0dced9bbe4beaee3dd5f8d000336fe0f76350d1
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-02 06:18:47 +0300

    Expense Line Chart
    prepareExpenseLineChartData in helper.js
    
    and the CustomToolTip is different, made new one in CustomLineChart.tsx

--- frontend/src/pages/Dashboard/Expense.tsx
+++ frontend/src/pages/Dashboard/Expense.tsx
@@ -1,8 +1,89 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import DashboardLayout from '../../components/layout/DashboardLayout'
+import toast from 'react-hot-toast';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import ExpenseOverview from '../../components/Expense/ExpenseOverview';
 
 const Expense = () => {
+  useUserAuth();
+
+  const [expenseData, setExpenseData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({ show: false, data: null });
+  const [OpenAddExpenseModal, setOpenAddExpenseModal] = useState(false);
+
+  //Get All Expense Details
+  const fetchExpenseDetails = async () => {
+    if (loading) return;
+    setLoading(true);
+
+    try {
+      const response = await axiosInstance.get(`${API_PATHS.EXPENSE.GET_ALL_EXPENSE}`);
+
+      if (response.data) {
+        setExpenseData(response.data);
+      }
+    } catch (error) {
+      console.log("Failed to fetch expense details. Please try again", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  //Handle Add Expense
+  const handleAddExpense = async (expense) => {
+    const { category, amount, date, icon } = expense;
+
+    //validation
+    if (!category.trim()) {
+      toast.error("Category field is required")
+      return;
+    }
+
+    if (!amount || isNaN(amount) || Number(amount) <= 0) {
+      toast.error("Amount must be a valid number greater than 0")
+      return;
+    }
+
+    if (!date) {
+      toast.error("Date is required")
+      return;
+    }
+
+    try {
+      await axiosInstance.post(`${API_PATHS.EXPENSE.ADD_EXPENSE}`, { category, amount, date, icon });
+
+      setOpenAddExpenseModal(false);
+      toast.success("Expense added successfully");
+      fetchExpenseDetails();
+    } catch (error) {
+      console.log("Failed to add expense. Please try again", error.response?.data?.message || error.message
+      );
+    }
+
+  }
+
+  useEffect(() => {
+    fetchExpenseDetails();
+    return () => { };
+  }, []);
   return (
-    <div>Expense</div>
+    <DashboardLayout activeMenu={"Expense"}>
+
+      <div className='my-5 mx-auto'>
+        <div className='grid grid-cols-1 gap-6'>
+          <div className=''>
+            {/* Line Chart */}
+            <ExpenseOverview
+              transactions={expenseData}
+              onExpenseIncome={() => setOpenAddExpenseModal(true)}
+            />
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
