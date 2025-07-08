b18efcf2ed93e38417210fbaed2a431e8e6ba6b0
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-09 00:10:34 +0300

    changed layout design right side

--- frontend/src/components/layout/AuthLayout.tsx
+++ frontend/src/components/layout/AuthLayout.tsx
@@ -6,14 +6,21 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex'>
             <div className='v-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-                <h2 className='text-2xl font-bold text-black'>Expense Tracker</h2>
+                <div className="flex items-center gap-2 mb-2">
+                    <span className="text-3xl text-primary drop-shadow-md"><LuTrendingUpDown /></span>
+                    <h2 className="text-4xl font-extrabold bg-gradient-to-r from-blue-700 via-sky-700 to-cyan-700 bg-clip-text text-transparent tracking-wide drop-shadow-lg select-none">Pocket Pilot</h2>
+                </div>
+                <p className="text-base italic text-gray-400 tracking-wide mb-6 pl-1 select-none">Your Personal Finance Assistant</p>
                 {children}
             </div>
 
-            <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
-                <div className='w-48 h-48 rounded-[40px] bg-primary absolute -top-7 -left-5' />
+            <div
+                className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'
+                style={{ backgroundImage: `url(${CARD_2})` }}
+            >
+                {/* <div className='w-48 h-48 rounded-[40px] bg-primary absolute -top-7 -left-5' />
                 <div className='w-48 h-48 rounded-[40px] border-[20px] border-blue-600/50 absolute top-[30%] -right-10' />
-                <div className='w-48 h-48 rounded-[40px] bg-primary/50 absolute -bottom-7 -left-5' />
+                <div className='w-48 h-48 rounded-[40px] bg-primary/50 absolute -bottom-7 -left-5' /> */}
 
                 <div className='grid grid-cols-1 z-20'>
                     <StatsInfoCard
@@ -24,11 +31,6 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
                     />
 
                 </div>
-
-                {/* <img
-                    src={CARD_2}
-                    className='w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15 rounded-' /> */}
-
             </div>
 
 
