2eabd7d140125377a7eb5b01fb9161e58d2cf5b9
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-18 17:34:32 +0300

    fixed all tooltip darkmode and btn

--- frontend/src/components/Charts/CustomBarChart.tsx
+++ frontend/src/components/Charts/CustomBarChart.tsx
@@ -13,7 +13,7 @@ const CustomBarChart = ({data}) => {
         if(active && payload && payload.length) {
             return (
                 <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300">
-                    <p className="text-xs font-semibold text-purple-800 mb-1">{payload[0].payload.category}</p>
+                    <p className="text-xs font-semibold text-primary mb-1">{payload[0].payload.category}</p>
                     <p className='text-sm text-gray-600'>
                         Amount: <span className='text-sm font-medium text-gray-900'>${payload[0].payload.amount}</span>
                     </p>
@@ -24,7 +24,7 @@ const CustomBarChart = ({data}) => {
     };
 
   return (
-    <div className='bg-white mt-6'>
+    <div className=' mt-6'>
         <ResponsiveContainer width={"100%"} height={300} >
             <BarChart data={data}>
                 <CartesianGrid stroke='none'/>
@@ -36,7 +36,7 @@ const CustomBarChart = ({data}) => {
                 <Bar
                     dataKey={"amount"}
                     fill='#FF8042'
-                    activeBar={{fill:'green', radius: 5, strokeWidth: 1, stroke: "yellow"}}
+                    activeBar={{radius: 5, strokeWidth: 1, stroke: "green"}}
                 >
                     {
                         data.map((entry, index) => (
