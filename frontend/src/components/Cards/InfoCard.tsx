d4c88ef1f6f4ce14cc4cd38ae1c55e8117f0b995
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-07-23 14:58:49 +0300

    added debt notifier

--- frontend/src/components/Cards/InfoCard.tsx
+++ frontend/src/components/Cards/InfoCard.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const InfoCard = ({icon, label, value,color}) => {
+const InfoCard = ({ icon, label, value, color }) => {
   return (
     <div className='card flex gap-6  p-6 rounded-2xl shadow-md  border'>
-        <div className={`w-14 h-14 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
+      <div className={`w-14 h-14 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
         {icon}
-        </div>
-        <div>
-            <h6 className='text-sm text-gray-500 mb-1 font-semibold'>{label}</h6>
-            <span className='text-[22px]'>{value}</span>
-        </div>
+      </div>
+      <div>
+        <h6 className='text-sm text-gray-500 mb-1 font-semibold'>{label}</h6>
+        <span className={`text-[22px] ${String(value).includes('-') ? 'text-red-500' : ''}`}>
+          {value}
+        </span>
+      </div>
     </div>
   )
 }
